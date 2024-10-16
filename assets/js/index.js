55d94b1e924ca353b2e541c2ba70cfe3b71b8e08
nghiakieran <lechinghia202@gmail.com>
2024-10-16 10:08:46 +0700

    Fist UI Header Menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,32 @@
+// Scroll fill color
 window.onscroll = () => {
-    if(scrollY > 233){
+    if(scrollY > 190){
         document.querySelector('.header').style.backgroundColor = '#1b1514e7';
     }
     else if (scrollY === 0){
         document.querySelector('.header').style.backgroundColor = 'transparent';
     }
+}
+// Open, off modal menu buy item 
+// Lấy các phần tử từ HTML
+let modal = document.getElementById("modal-root");
+let link = document.getElementById("add-cart");  // Thẻ link 'Đặt mua'
+let closeBtn = document.querySelector(".close");  // Nút đóng modal
+
+// Khi click vào thẻ a "Đặt mua", modal sẽ hiện lên
+link.onclick = function(event) {
+  event.preventDefault();  // Ngăn chặn điều hướng mặc định của thẻ a
+  modal.style.display = "block";  // Hiển thị modal
+}
+
+// Khi click vào nút "X", modal sẽ bị ẩn
+closeBtn.onclick = function() {
+  modal.style.display = "none";  // Ẩn modal
+}
+
+// Khi người dùng click ra ngoài modal, modal cũng sẽ bị ẩn
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
---
+++ assets/js/slider.js
@@ -0,0 +1,33 @@
+
+// for menu slider
+$(document).ready(function () {
+    $(".image-slider").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      // autoplay: true,
+      // autoplaySpeed: 4000,
+    });
+  });
+  
