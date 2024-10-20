ea11ba1fde55f01b33dc33b61775935a04b78d35
nghiakieran <lechinghia202@gmail.com>
2024-10-20 16:25:31 +0700

    First UI Modal-Menu Login

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,26 +7,3 @@ window.onscroll = () => {
         document.querySelector('.header').style.backgroundColor = 'transparent';
     }
 }
-// Open, off modal menu buy item 
-// Lấy các phần tử từ HTML
-let modal = document.getElementById("modal-root");
-let link = document.getElementById("add-cart");  // Thẻ link 'Đặt mua'
-let closeBtn = document.querySelector(".close");  // Nút đóng modal
-
-// Khi click vào thẻ a "Đặt mua", modal sẽ hiện lên
-link.onclick = function(event) {
-  event.preventDefault();  // Ngăn chặn điều hướng mặc định của thẻ a
-  modal.style.display = "block";  // Hiển thị modal
-}
-
-// Khi click vào nút "X", modal sẽ bị ẩn
-closeBtn.onclick = function() {
-  modal.style.display = "none";  // Ẩn modal
-}
-
-// Khi người dùng click ra ngoài modal, modal cũng sẽ bị ẩn
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
---
+++ assets/js/login.js
@@ -0,0 +1,78 @@
+
+// IIFE
+(function ($) {
+    "use strict";
+    // input to button
+    $('.button').on('click', function(){
+        $('.validate-form').submit();
+    });
+    
+     /*==================================================================
+    [ Focus input ]*/
+    // $('.input').each(function(){
+    //     $(this).on('blur', function(){
+    //         if($(this).val().trim() != "") {
+    //             $(this).addClass('has-val');
+    //         }
+    //         else {
+    //             $(this).removeClass('has-val');
+    //         }
+    //     })    
+    // })
+  
+    /*==================================================================
+    [ Validate ]*/
+    let input = $('.validate-input .input');
+    // Xử lý sự kiện submit của form
+    $('.validate-form').on('submit', function (event) {
+        let check = true;
+    
+        for (let i = 0; i < input.length; i++) {
+            if (validate(input[i]) === false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+        // // Ngăn chặn hành động submit mặc định nếu có lỗi
+        // if (!check) {
+        //     event.preventDefault();
+        // }
+    
+        return check;
+    });
+
+    // Xử lý khi input được focus
+    $('.validate-form .input').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    // Kiểm tra validate
+    function validate (input) {
+        // if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        //     if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        //         return false;
+        //     }
+        // }
+        if ($(input).val().trim() === '') {
+            return false;
+        }
+       // return true;
+    }
+    // Hiển thị lỗi
+    function showValidate(input) {
+        let thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+    //  Ẩn lỗi
+    function hideValidate(input) {
+        let thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
